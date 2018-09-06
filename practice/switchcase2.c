80894ca98ba95fb073bf885d0846e2d01a057a4f
dbc2201 <divyanshb22@gmail.com>
2018-09-06 13:33:48 +0530

    Add files via upload

---
+++ practice/switchcase2.c
@@ -0,0 +1,190 @@
+/*
+*   Author      :   Rachit Agrawal, B.Tech 1st Year, Section C, GLA University
+*   Aim         :   To demonstrate the use of Switch Case
+*/
+#include <stdio.h>
+void main()
+{
+    char a;
+	printf("Enter first letter of your name (only upper case) :-\n");
+	scanf("%c", &a);
+    switch(a)
+    {
+        case 'A' :
+                    printf("Bill Gates\nBusiness Magnate, Investor & Philanthropist\nIts fine to celebrate success but it is more important to heed the lessons of failure.\n");
+                    break;
+        case 'B' :
+                    printf("Donald Trump\nAmerican Business Magnate, Investor & TV Personality\nPart of being a winner is knowing when enough is enough. Sometimes you have to give up the fight and walk away, and move on to something thats more productive.\n");
+                    break;
+        case 'C' :
+                    printf("Cyril Ramaphosa\nSouth African Politician & Businessman\nNo action is too small when it comes to changing the world Im inspired every time I meet an entrepreneur who is succeeding against all odds.\n");
+                    break;
+        case 'D' :
+                    printf("J. Paul Getty\nFounder of Getty Oil Company\nFormula for success: rise early, work hard, strike oil.\n");
+                    break;
+        case 'E' :
+                    printf("Li Ka-Shing\nHong Kong Business Magnate & Philanthropist\nVision is perhaps our greatest strength it has kept us alive to the power and continuity of thought through the centuries, it makes us peer into the future and lends shape to the unknown.\n");
+                    break;
+        case 'F' :
+                    printf("Andrew Carnegie\nSteel Magnate\nAs I grow older, I pay less attention to what men say. I just watch what they do.\n");
+                    break;
+        case 'G' :
+                    printf("Oprah Winfrey\nTalk Show Host, Producer & Philanthropist\nWhat other people label or might try to call failure, I have learned is just Gods way of pointing you in a new direction.\n");
+                    break;
+        case 'H' :
+                    printf("Eike Batista\nBrazilian Business Magnate\nDont think you are unstoppable or foolproof. Dont think that the only way your business will work is through perfection. Dont aim for perfection. Aim for success.\n");
+                    break;
+        case 'I' :
+                    printf("Sheldon Adelson\nAmerican Business Magnate\nFor me, businesses are like buses. You stand on a corner and you dont like where the first bus is going? Wait ten minutes and take another. Dont like that one? Theyll just keep coming. Theres no end to buses or businesses.\n");
+                    break;
+        case 'J' :
+                    printf("Tony Hsieh\nFounder of Zappos\nHave fun. The game is a lot more enjoyable when youre trying to do more than just make money.\n");
+                    break;
+        case 'K' :
+                    printf("Henry Ford\nFounder of Ford Motors\nIf you think you can do a thing or think you cant do a thing, youre right.\n");
+                    break;
+        case 'L' :
+                    printf("Jeff Bezos\nFounder of Amazon\nIf you never want to be criticized, for goodness sake dont do anything new.\n");
+                    break;
+        case 'M' :
+                    printf("Lakshmi Mittal\nIndian Steel Magnate\nHard work certainly goes a long way. These days a lot of people work hard, so you have to make sure you work even harder and really dedicate yourself to what you are doing and setting out to achieve.\n");
+                    break;
+        case 'N' :
+                    printf("John D Rockefeller\nFounder of  the Standard Oil Company & Philanthropist\nIf you want to succeed you should strike out on new paths, rather than travel the worn paths of accepted success.\n");
+                    break;
+        case 'O' :
+                    printf("Michael Bloomberg\nMayor of New York, Investor & Entrepreneur\nGetting the job done has been the basis for the success my company has achieved.\n");
+                    break;
+        case 'P' :
+                    printf("Mark Zuckerberg\nFounder & CEO of Facebook\nThe biggest risk is not taking any risk In a world that changing really quickly, the only strategy that is guaranteed to fail is not taking risks.\n");
+                    break;
+        case 'Q' :
+                    printf("Stefan Persson\nFounder of H&M\nLoyalty is not won by being first. It is won by being best.\n");
+                    break;
+        case 'R' :
+                    printf("Chuck Feeney\nBusinessman & Philanthropist\nThere has to be a balance in life. A balance of business, family, and the opportunity to learn and teach.\n");
+                    break;
+        case 'S' :
+                    printf("Steve Jobs\nFounder of Apple\nDont let the noise of others opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.\n");
+                    break;
+        case 'T' :
+                    printf("Azim Premji\nIndian Businessman & Philanthropist\nIf people are not laughing at your goals, your goals are too small.\n");
+                    break;
+        case 'U' :
+                    printf("Richard Branson\nFounder of Virgin\nA business has to be involving, it has to be fun, and it has to exercise your creative instincts.\n");
+                    break;
+        case 'V' :
+                    printf("Ted Turner\nAmerican Media Mogul\nYou can never quit. Winners never quit, and quitters never win.\n");
+                    break;
+        case 'W' :
+                    printf("Mark Cuban\nBillionaire Shark Tank Investor & Owner of The Dallas Mavericks\nSweat equity is the most valuable equity there is. Know your business and industry better than anyone else in the world. Love what you do or dont do it.\n");
+                    break;
+        case 'X' :
+                    printf("Clive Palmer\nAustralian Businessman & Politician\nI realised that in a lot of failures, there is a lot of opportunities.\n");
+                    break;
+        case 'Y' :
+                    printf("Ingvar Kamprad\nFounder of IKEA\nThe most dangerous poison is the feeling of achievement. The antidote is to every evening think what can be done better tomorrow.\n");
+                    break;
+        case 'Z' :
+                    printf("Elon Musk\nFounder of PayPal, Tesla Motors & SpaceX\nI think thats the single best piece of advice: constantly think about how you could be doing things better and questioning yourself.\n");
+                    break;
+        default  :  printf("You have entered wrong format.\n");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
