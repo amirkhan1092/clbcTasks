dd4b6ead506ced31611d46f9154d90ba6405cb3a
dbc2201 <divyanshb22@gmail.com>
2018-09-14 00:51:08 +0530

    Add files via upload

---
+++ practice/whitespace.c
@@ -0,0 +1,16 @@
+/*
+*   File        :       whitespace.c
+*   Author      :       Divyansh Bhardwaj, Technical Trainer, GLA University
+*   Aim         :       To demonstrate different whitespaces in C
+*/
+#include<stdio.h>
+int main()
+{
+    printf("This is space |' '|\n");
+    printf("This is horizontal tab |'\t'|\n");
+    printf("This is newline |'\n'|\n");
+    printf("This is vertical tab |'\v'|\n");
+    printf("This is feed |'\f'|\n");
+    printf("This is carriage return |'\r'|\n");
+    return 0;
+}
